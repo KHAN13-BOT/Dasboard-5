8c61b06b847a50a863c6e53ca9f5cbcb07026573
AFRAZ KHAN JADOON <afrazjadoon7@gmail.com>
2026-05-15 15:16:48 +0500

    Add files via upload

---
+++ Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <div className='bg-blue-600 w-64 text-white h-screen'>
+      
+<li>Home</li>
+      <li>User</li>
+<li>Setting</li>
+    </div>
+  )
+}
